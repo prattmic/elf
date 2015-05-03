6ad0150ef4bf518374f3acdd1cde590e54cdef77
Michael Pratt <michael@pratt.im>
2015-05-03 10:04:02 -0700

    First test
    Pretend to read first word from file, determine it is an ELF.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
-#[test]
-fn it_works() {
+pub fn first_word(file: &'static str) -> u64 {
+    // TODO: read from file!
+    0x464c457f  // .ELF
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn detect_elf() {
+        assert_eq!(0x464c457f, first_word("/proc/self/exe"))
+    }
 }
