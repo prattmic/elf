b4b42db53d103c0a928db78efe2a4c72976ab3d9
Michael Pratt <michael@pratt.im>
2015-05-03 11:22:43 -0700

    Create Elf struct
    Open the file, make sure it has elf magic

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,28 @@ use std::io;
 use std::io::Read;
 use std::path::Path;
 
-pub fn first_word<F: AsRef<Path>>(file: F) -> io::Result<u32> {
-    let mut f = try!(File::open(file));
+pub struct Elf {
+    file: File,
+}
+
+impl Elf {
+    fn open<F: AsRef<Path>>(filename: F) -> io::Result<Elf> {
+        let mut f = try!(File::open(filename));
+        let first_word = try!(first_word(&mut f));
+
+        if first_word != 0x464c457f {  // .ELF
+            return Err(io::Error::new(io::ErrorKind::Other, "Not an ELF file"));
+        }
+
+        Ok(Elf{file: f})
+    }
+}
+
+pub fn first_word(file: &mut File) -> io::Result<u32> {
     let mut buf: [u8; 4] = [0, 0, 0, 0];
 
     // Read the first 4 bytes
-    let size = try!(f.read(&mut buf));
+    let size = try!(file.read(&mut buf));
     if size != 4 {
         return Err(io::Error::new(io::ErrorKind::Other, "Unable to read 4 bytes"));
     }
@@ -23,14 +39,26 @@ pub fn first_word<F: AsRef<Path>>(file: F) -> io::Result<u32> {
 #[cfg(test)]
 mod test {
     use super::*;
+    use std::fs::File;
+
+    // Borrowed from https://github.com/rust-lang/rust/blob/master/src/libstd/fs.rs
+    macro_rules! check { ($e:expr) => (
+        match $e {
+            Ok(t) => t,
+            Err(e) => panic!("{} failed with: {}", stringify!($e), e),
+        }
+    ) }
 
     #[test]
     fn detect_elf() {
-        let word = first_word("/proc/self/exe");
+        let mut f = check!(File::open("/proc/self/exe"));
+        let word = check!(first_word(&mut f));
 
-        match word {
-            Ok(w) => assert_eq!(0x464c457f, w), // .ELF
-            Err(e) => panic!("failed with: {}", e),
-        }
+        assert_eq!(0x464c457f, word) // .ELF
+    }
+
+    #[test]
+    fn open_elf() {
+        check!(Elf::open("/proc/self/exe"));
     }
 }
