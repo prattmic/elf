3934d53eb15557c5315305727b509326bcb62809
Michael Pratt <michael@pratt.im>
2015-05-09 20:26:30 -0700

    Move all parsing into Elf impl
    Use pread for reading.

--- src/elf.rs
+++ src/elf.rs
@@ -1,8 +1,9 @@
 use std::convert::AsRef;
 use std::fs::File;
 use std::io;
-use std::io::Read;
 use std::path::Path;
+
+use pread::Pread;
 
 const ELF_MAGIC: u32 = 0x464c457f;  // .ELF
 
@@ -12,30 +13,33 @@ pub struct Elf {
 
 impl Elf {
     pub fn open<F: AsRef<Path>>(filename: F) -> io::Result<Elf> {
-        let mut f = try!(File::open(filename));
-        let first_word = try!(first_word(&mut f));
+        let f = try!(File::open(filename));
+        let mut elf = Elf{file: f};
 
-        if first_word != ELF_MAGIC {
+        let valid = try!(elf.check_magic());
+        if !valid {
             return Err(io::Error::new(io::ErrorKind::Other, "Not an ELF file"));
         }
 
-        Ok(Elf{file: f})
-    }
-}
-
-fn first_word(file: &mut File) -> io::Result<u32> {
-    let mut buf = [0u8; 4];
-
-    // Read the first 4 bytes
-    let size = try!(file.read(&mut buf));
-    if size != 4 {
-        return Err(io::Error::new(io::ErrorKind::Other, "Unable to read 4 bytes"));
+        Ok(elf)
     }
 
-    Ok((buf[0] as u32) |
-       ((buf[1] as u32) << 8) |
-       ((buf[2] as u32) << 16) |
-       ((buf[3] as u32) << 24))
+    fn check_magic(&mut self) -> io::Result<bool> {
+        let mut buf = [0u8; 4];
+
+        // Read the first 4 bytes
+        let size = try!(self.file.pread(&mut buf, 0));
+        if size != 4 {
+            return Err(io::Error::new(io::ErrorKind::Other, "Unable to read 4 bytes"));
+        }
+
+        let magic = (buf[0] as u32) |
+                    ((buf[1] as u32) << 8) |
+                    ((buf[2] as u32) << 16) |
+                    ((buf[3] as u32) << 24);
+
+        Ok(magic == ELF_MAGIC)
+    }
 }
 
 #[cfg(test)]
@@ -48,5 +52,12 @@ mod test {
     #[test]
     fn open_elf() {
         check!(Elf::open("/proc/self/exe"));
+    }
+
+    #[test]
+    fn open_non_elf() {
+        if let Ok(_) = Elf::open("/proc/self/maps") {
+            panic!("Non-ELF opened successfully!");
+        }
     }
 }
